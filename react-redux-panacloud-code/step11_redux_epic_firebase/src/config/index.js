d5b4b139a3ebd5443c5e10402b099882e2d13d06
harisjamal2k13 <harisjamal2k13@gmail.com>
2018-01-10 14:14:05 +0500

    React-redux

---
+++ react-redux-panacloud-code/step11_redux_epic_firebase/src/config/index.js
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: ""
+}
+firebase.initializeApp(firebaseConfig);
